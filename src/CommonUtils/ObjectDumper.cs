06ca8b915843315643d6def6a349f756cfe1f0c6
cfvbaibai <893963665@qq.com>
2014-06-10 01:36:13 +0800

    Move ObjectDumper into CommonUtils

---
+++ src/CommonUtils/ObjectDumper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cfvbaibai.CommonUtils
+{
+    public static class ObjectDumper
+    {
+        public static string ShallowDumpProperties(object obj, int indentCount = 0)
+        {
+            if (obj == null)
+            {
+                return "<NULL>";
+            }
+            var indent = new string(' ', indentCount);
+            var result = new StringBuilder();
+            Type type = obj.GetType();
+            foreach (var propertyInfo in type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                var name = propertyInfo.Name;
+                var value = propertyInfo.GetValue(obj);
+                result.AppendFormat("{0}{1} = {2}", indent, name, value);
+                result.AppendLine();
+            }
+            return result.ToString();
+        }
+    }
+}
